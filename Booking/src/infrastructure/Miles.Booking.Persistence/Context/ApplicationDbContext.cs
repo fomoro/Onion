02acafbf8ab3b85a22037b2000fa5e3507a8098f
Jonathan Wolfan <wmoreno@viajemos.com>
2022-06-17 17:35:38 -0500

    se configura persistencia

---
+++ Booking/src/infrastructure/Miles.Booking.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Miles.Booking.Domain.Entities;
+
+namespace Miles.Booking.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Booking/src/infrastructure/Miles.Booking.Persistence/Repository/MyRepositoryAsync.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Miles.Booking.Domain.Interfaces;
+using Miles.Booking.Persistence.Context;
+
+namespace Miles.Booking.Persistence.Repository
+{
+    public class MyRepositoryAsync<T> : RepositoryBase<T>, IRepositoryAsync<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public MyRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
