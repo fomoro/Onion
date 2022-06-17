5ed80100681392a3da2a90bab1e663eb99764050
Jonathan Wolfan <wmoreno@viajemos.com>
2022-06-17 15:23:14 -0500

    se crean los proyectos del proyecto onion

---
+++ Booking/src/domain/Miles.Booking.Domain/DTOs/CustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles.Booking.Domain.DTOs
+{
+    public class CustomerDto
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Booking/src/domain/Miles.Booking.Domain/Entities/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles.Booking.Domain.Entities
+{
+    public class Customer
+    {
+        public virtual int Id { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+
+        private int _edad;
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+
+        public int Edad
+        {
+            get
+            {
+                if (this._edad <= 0)
+                {
+                    this._edad = new DateTime(DateTime.Now.Subtract(this.FechaNacimiento).Ticks).Year - 1;
+                }
+                return this._edad;
+            }
+            set
+            {
+                this._edad = value;
+            }
+        }
+    }
+}
